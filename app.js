ecce4554df1c8c8f1ed40b790805cf5179011a8c
haln <laqueenster@hotmail.com>
2020-06-23 12:15:19 -0700

    Add listener

--- app.js
+++ app.js
@@ -98,6 +98,8 @@ app.get('/auth/steam/return',
     res.redirect('/account');
   });
 
+app.listen(3000);
+
 // Simple route middleware to ensure user is authenticated.
 //   Use this route middleware on any resource that needs to be protected.  If
 //   the request is authenticated (typically via a persistent login session),
